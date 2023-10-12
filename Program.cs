46f717c1376ce8e1bdf85d0764ccfd6c232d509a
Paweł Kurek <pawel.kurek975@gmail.com>
2023-10-12 21:28:29 +0200

    phonebook final

--- Program.cs
+++ Program.cs
@@ -1,2 +1,106 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Phonebook
+{
+    class Program
+    {
+        static Dictionary<string, string> contacts = new Dictionary<string, string>();
+
+        static public void AddContact(string contactName, string contactNumber)
+        {
+            if (!contacts.ContainsKey(contactName))
+            {
+                contacts[contactName] = contactNumber;
+            }
+            else
+            {
+                Console.WriteLine($"Contact with name {contactName} is already in phonebook");
+            }
+        }
+
+        static public void showContactByNumber(string contactNumber)
+        {
+            string contactName = null;
+
+            foreach (var name in contacts.Keys)
+            {
+                if (contacts[name] == contactNumber)
+                {
+                    contactName = name;
+                    break;
+                }
+            }
+
+            if (contactName != null)
+            {
+                Console.WriteLine($"Name: {contactName}, Number: {contacts[contactName]}");
+            }
+            else
+            {
+                Console.WriteLine($"There is no contact with name: {contactName}");
+            }
+        }
+
+        static public void showContactByName(string contactName)
+        {
+            if (contacts.ContainsKey(contactName))
+            {
+                Console.WriteLine($"Name: {contactName}, Number: {contacts[contactName]}");
+            }
+            else
+            {
+                Console.WriteLine($"There is no contact with name: {contactName}");
+            }
+        }
+
+        static public void showAllContacts()
+        {
+            foreach (var contactName in contacts.Keys)
+            {
+                Console.WriteLine($"Name: {contactName}, Number: {contacts[contactName]}");
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("*** Phonebook ***");
+
+            Console.WriteLine("Type 'add-contact' to add contact to phonebook");
+            Console.WriteLine("Type 'show-all-contacts' to show all contacts");
+            Console.WriteLine("Type 'show-contact-by-name' to show contact by name");
+            Console.WriteLine("Type 'show-contact-by-number' to show contact by number");
+
+            var actionType = Console.ReadLine();
+
+            if (actionType == "add-contact")
+            {
+                Console.WriteLine("Name: ");
+                var contactName = Console.ReadLine();
+                Console.WriteLine("Number: ");
+                var contactNumber = Console.ReadLine();
+                if (contactNumber != null && contactName != null) AddContact(contactName, contactNumber);
+            }
+            else if (actionType == "show-all-contacts") showAllContacts();
+            else if (actionType == "show-contact-by-name")
+            {
+                Console.WriteLine("Name: ");
+                var contactName = Console.ReadLine();
+                if (contactName != null) showContactByName(contactName);
+            }
+            else if (actionType == "show-contact-by-number")
+            {
+                Console.WriteLine("Number: ");
+                var contactNumber = Console.ReadLine();
+                if (contactNumber != null) showContactByNumber(contactNumber);
+            }
+            else
+            {
+                Console.WriteLine("Invalid action...");
+            }
+
+
+
+
+        }
+    }
+}
+}
